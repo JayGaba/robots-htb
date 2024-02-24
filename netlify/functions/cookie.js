a1cb06d108bbb6b66e70f55020fd240604a6fd82
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2024-02-24 19:12:27 +0530

    Update cookie.js

--- netlify/functions/cookie.js
+++ netlify/functions/cookie.js
@@ -1,34 +1,32 @@
 exports.handler = async (event, context) => {
-  // Set the default cookie value
   let cookieValue = '1';
   
-  // Check if the cookie is already set
   if (event.headers.cookie && event.headers.cookie.includes("value=")) {
-    // Extract the cookie value
     cookieValue = event.headers.cookie
       .split('; ')
       .find(cookie => cookie.startsWith('value='))
       .split('=')[1];
   }
 
-  // Set response body and redirection if the cookie value is '69'
+  console.log(`Current cookie value: ${cookieValue}`); // Added for debugging
+
   let redirectLocation = '';
   if (cookieValue === '69') {
     redirectLocation = '/lmaotaketheflag.txt';
   } else {
-    // If the cookie value is not '69', reset it to '1'
     cookieValue = '1';
   }
 
-  // Set the cookie and return the redirect if the cookie value is '69', otherwise, just set the cookie
+  console.log(`Redirect location: ${redirectLocation}`); // Added for debugging
+
   const response = {
-    statusCode: 302, // Redirect status code
+    statusCode: 302,
     headers: {
       'Content-Type': 'text/plain',
       'Set-Cookie': `value=${cookieValue}; Path=/; Max-Age=86400; SameSite=None; Secure`,
-      'Location': redirectLocation // Include the redirect location in the headers
+      'Location': redirectLocation
     },
-    body: '' // Empty response body
+    body: ''
   };
 
   return response;
